60032c9eebba03b97e5ab10e80bed9b7ff2d2b56
acepW <acepahyu001@gmail.com>
2024-10-09 19:29:30 +0700

    update constant colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFFF8F00);
+const Color accentColor = Color(0xFFF44336);
+const Color backgroundColor = Color(0xFFEEEEEE);
+const Color blackColor = Color.fromARGB(255, 0, 0, 0);
+const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
