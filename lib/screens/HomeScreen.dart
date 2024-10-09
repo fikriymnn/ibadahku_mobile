30c156b0cabd8dc300e8d4d7e27b77889d05c9f3
acepW <acepahyu001@gmail.com>
2024-10-09 19:38:40 +0700

    add appbar in home screen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ibadahku_mobile/constants/colors.dart';
+import 'package:ibadahku_mobile/widgets/textWidgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +9,28 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Text("data"),
+        centerTitle: false,
+        title: TextWidget(
+          text: "Ibadahku",
+          color: whiteColor,
+          textSize: 20,
+          isTitle: true,
+        ),
+        backgroundColor: primaryColor,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: whiteColor,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                color: whiteColor,
+              )),
+        ],
       ),
       body: Container(),
     );
