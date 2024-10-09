a7484b066d9ad7c5ea0d359017510de3e357b0ef
acepW <acepahyu001@gmail.com>
2024-10-09 22:37:20 +0700

    unpdate

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:ibadahku_mobile/constants/colors.dart';
+import 'package:ibadahku_mobile/services/sholatServices.dart';
 import 'package:ibadahku_mobile/widgets/textWidgets.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _loading = true;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  getJadwal() async {
+    dynamic jadwalSholat = await JadwalSholat().getJadwalSholat();
+    return jadwalSholat;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +51,86 @@ class HomeScreen extends StatelessWidget {
               )),
         ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width * 0.40,
+            decoration: BoxDecoration(color: accentColor),
+            child: Center(
+              child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: FutureBuilder(
+                    future: getJadwal(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Text("");
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (snapshot.hasData) {
+                        print(snapshot.data["data"]);
+                        String namaWaktu = "";
+                        String targetTimeStringMidnight =
+                            snapshot.data["data"]["timings"]["Midnight"];
+                        String targetTimeStringFajr =
+                            snapshot.data["data"]["timings"]["Fajr"];
+                        String targetTimeStringIsha =
+                            snapshot.data["data"]["timings"]["Isha"];
+                        DateTime now = DateTime.now();
+                        String currentTime = now.toString().substring(11, 16);
+
+                        DateFormat formatter = DateFormat('HH:mm');
+                        DateTime targetTimeMidnight =
+                            formatter.parse(targetTimeStringMidnight);
+                        DateTime targetTimeFajr =
+                            formatter.parse(targetTimeStringFajr);
+                        DateTime targetTimeIsha =
+                            formatter.parse(targetTimeStringIsha);
+                        if (now.isAfter(targetTimeIsha) &&
+                            now.isBefore(targetTimeMidnight)) {
+                          namaWaktu = "Subuh";
+                          print(namaWaktu);
+                        } else {
+                          namaWaktu = "salah";
+                        }
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextWidget(
+                                text: "Subuh", color: whiteColor, textSize: 15),
+                            TextWidget(
+                                text: "04:13", color: whiteColor, textSize: 30),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  TextWidget(
+                                      text:
+                                          "${snapshot.data["data"]["date"]["hijri"]["day"]} ${snapshot.data["data"]["date"]["hijri"]["month"]["en"]} ${snapshot.data["data"]["date"]["hijri"]["year"]}",
+                                      color: whiteColor,
+                                      textSize: 15),
+                                  TextWidget(
+                                      text:
+                                          "${snapshot.data["data"]["date"]["gregorian"]["weekday"]["en"]} ${snapshot.data["data"]["date"]["gregorian"]["day"]} ${snapshot.data["data"]["date"]["gregorian"]["month"]["en"]}",
+                                      color: whiteColor,
+                                      textSize: 15)
+                                ],
+                              ),
+                            )
+                          ],
+                        );
+                      }
+                      return Text('Data tidak ditemukan');
+                    },
+                  )),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/services/sholatServices.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+
+class JadwalSholat {
+  final dio = Dio();
+
+  String formattedDate = DateFormat('dd-MM-yyyy').format(DateTime.now());
+
+  Future<Position> getPermisionLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future getJadwalSholat() async {
+    Position myLocation = await getPermisionLocation();
+    DateTime now = DateTime.now();
+    dynamic timeZoneName = now.timeZoneName;
+    print(myLocation);
+    print(timeZoneName);
+    print(formattedDate);
+    try {
+      final response = await dio.get(
+        'http://api.aladhan.com/v1/timings/${formattedDate}?latitude=${myLocation.latitude}&longitude=${myLocation.longitude}&timezonestring=Europe/London&method=20',
+      );
+      //print(response.data["data"]);
+      return response.data;
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
