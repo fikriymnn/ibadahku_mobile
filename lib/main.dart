beac4e60962035e309f2e056a37e6777de75169f
Fikri Zaki Aditama, S.Si <fikriyomann@gmail.com>
2024-10-09 19:37:46 +0700

    change primary color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ibadahku_mobile/constants/colors.dart';
 import 'package:ibadahku_mobile/screens/BottomBarScreen.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Ibadahku Mobile',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
       ),
       home: const BottomBarScreen(),
