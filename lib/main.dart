7fdeea4ad89f48e9d80f8b342d7e31a4cddc693b
Fikri Zaki Aditama, S.Si <fikriyomann@gmail.com>
2024-10-09 19:29:40 +0700

    bottom bar nav waterdrop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ibadahku_mobile/screens/HomeScreen.dart';
+import 'package:ibadahku_mobile/screens/BottomBarScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const BottomBarScreen(),
     );
   }
 }
