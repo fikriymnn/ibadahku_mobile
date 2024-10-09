5ae9ac0a823ccb76a1e258ee758a8f857f69a290
acepW <acepahyu001@gmail.com>
2024-10-09 19:29:30 +0700

    update constant colors

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -6,6 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Text("data"),
+      ),
       body: Container(),
     );
   }
