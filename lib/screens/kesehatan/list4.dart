b0c0264eb9a4c63eb47e3e2f6361b3b4532e0ef2
Fikri Zaki Aditama, S.Si <fikriyomann@gmail.com>
2024-10-12 15:11:12 +0700

    update list4

--- lib/screens/kesehatan/list4.dart
+++ lib/screens/kesehatan/list4.dart
@@ -98,7 +98,139 @@ class List4 extends StatelessWidget {
               "Selain itu biasakan latihan kesegaran jasmani dengan melakukan senam kesegaran jasmani.",
               "Bagi calon jemaah yang mempunyai resiko penyakit atau sudah menderita penyakit seperti hipertensi, diabetes hendaknya berkonsultasi dengan petugas kesehatan sebelum melkukan latihan.",
             ],
-          )
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "4. Latihan penyesuaian terhadap musim dingin di Arab Saudi",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          BulletedList(
+            bulletColor: blackColor,
+            style: TextStyle(
+              color: blackColor,
+              fontSize: 15,
+            ),
+            listItems: [
+              "Perlu diperhatikan perubahan suhu cukup ekstrim dari Indonesia ke Arab Saudi.",
+              "Pada musim dingin suhu dapat mencapai nol derajat dengan kelembaban udara 24% (sangat kering).",
+              "Lakukan latihan penyesuaian pada dini hari sesudah subuh, di tempat terbuka, di dataran tinggi atau pegunungan dengan melakukan latihan kebugaran jasmani dan jalan kaki setiap hari selama 1 jam selama 1 pekan pada pekan terakhir menjelang keberangkatan.",
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "5. Pengaturan berat badan dengan gizi seimbang",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          BulletedList(
+            bulletColor: blackColor,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: blackColor,
+            ),
+            listItems: [
+              "Berat badan yang ideal dapat mempertahankan kondisi kesehatan yang optimal.",
+              "Perlu pengaturan menu gizi seimbang yang terdiri dari karbohidrat, protein, susu sayur dan buah.",
+              "Perbanyak makan sayuran hijau.",
+              "Makan buah yang banyak mengandung vitamin seperti jeruk, jambu biji.",
+              "Pilih makanan rendah lemak, batasi yang mengandung gula dan garam.",
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "6. Bawa obat pribadi yang diperlukan",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          BulletedList(
+            bulletColor: blackColor,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: blackColor,
+            ),
+            listItems: [
+              "Bawa obat ringan mengatasi batuk pilek, luka ringan atau diare. Pilih yang praktis dibawa, tidak menyebabkan kantuk.",
+              "Bila menderita penyakit tertentu yang membutuhkan pengobatan terus menerus seperti hipertensi, diabetes, siapkan obat untuk penyakit tersebut sejumlah kebutuhan selama perjalanan haji.",
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "7. Bawa obat pribadi yang diperlukan",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          BulletedList(
+            bulletColor: blackColor,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15,
+              color: blackColor,
+            ),
+            listItems: [
+              "Sengatan dingin dapat menggagnggu tubuh mulai ringan sampai berat dengan tanda-tanda kulit gatal, menggigil, gigi gemeretak, rasa kaku, kram, kaku otot, mati rasa, sulit konsentrasi, bicara tidak jelas, sudah bernafas, kesadaran menurun.",
+              "Bila mengalami seperti tanda di atas segera hubungi petugas kesehatan.",
+            ],
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            "8. Kesiapan menghadapi musim panas",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          BulletedList(
+            bulletColor: blackColor,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15,
+              color: blackColor,
+            ),
+            listItems: [
+              "Perbanyak minum air putih atau air zam-zam untuk menghindari dehidrasi",
+              "Gunakan pelembab muka dan tubuh, mengurangi sengatan matahari.",
+              "Gunakan kacamata hitam, payung untuk melindungi dari sinar matahari.",
+              "Gunakan pelembab bibir agar tidak pecah-pecah",
+              "Pakai pakaian yang nyaman dan berwarna gelap.",
+              "Pakai alas kaki yang nyaman dan ringan.",
+            ],
+          ),
         ],
       ),
     );
