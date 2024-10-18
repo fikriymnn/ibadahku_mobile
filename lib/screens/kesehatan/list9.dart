e608ed42d42c4f8388f635c1ca3d98efe1847a1d
Fikri Zaki Aditama, S.Si <fikriyomann@gmail.com>
2024-10-18 14:51:29 +0700

    done

---
+++ lib/screens/kesehatan/list9.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+import '../../widgets/textWidgets.dart';
+
+class List9 extends StatelessWidget {
+  const List9({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: TextWidget(
+          text: "Kesehatan",
+          color: whiteColor,
+          textSize: 20,
+          isTitle: true,
+        ),
+        backgroundColor: primaryColor,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: whiteColor,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.menu,
+                color: whiteColor,
+              )),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: const [
+          Text(
+            "Haid/Menstruasi",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
