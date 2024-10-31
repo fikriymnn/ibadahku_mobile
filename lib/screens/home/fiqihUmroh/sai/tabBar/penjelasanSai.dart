8cc170d97b2e1257317a9f13dd488a20f2b3184e
Fikri Zaki Aditama, S.Si <fikriyomann@gmail.com>
2024-11-01 03:31:33 +0700

    add navigation to Rukun Sai

--- lib/screens/home/fiqihUmroh/sai/tabBar/penjelasanSai.dart
+++ lib/screens/home/fiqihUmroh/sai/tabBar/penjelasanSai.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ibadahku_mobile/screens/home/fiqihUmroh/sai/pengertianSai.dart';
+import 'package:ibadahku_mobile/screens/home/fiqihUmroh/sai/saiRukunUmroh.dart';
 import 'package:ibadahku_mobile/screens/home/fiqihUmroh/sai/sejarahSai.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
@@ -39,7 +40,14 @@ class PenjelasanSai extends StatelessWidget {
       {
         'icon': const FaIcon(FontAwesomeIcons.kaaba),
         'title': "Rukun Haji & Umroh",
-        'click': () {},
+        'click': () {
+          pushWithoutNavBar(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SaiRukunUmroh(),
+            ),
+          );
+        },
       },
       {
         'icon': const FaIcon(FontAwesomeIcons.bookOpenReader),
