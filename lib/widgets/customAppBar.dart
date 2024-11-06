0438310613cc86badd19c41229d63d06613462b1
Fikri Zaki Aditama, S.Si <fikriyomann@gmail.com>
2024-11-06 21:04:04 +0700

    add custom appbar

---
+++ lib/widgets/customAppBar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+import 'textWidgets.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final double height;
+  const CustomAppbar({super.key, required this.title, required this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: false,
+      title: TextWidget(
+        text: title,
+        color: whiteColor,
+        textSize: 20,
+        isTitle: true,
+      ),
+      backgroundColor: primaryColor,
+      /* actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: whiteColor,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                color: whiteColor,
+              )),
+        ], */
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(height);
+}
